8aacced9463a1b652ef6a4ee57cacd4aebb6a949
emceelamb <mlam@berkeley.edu>
2019-03-24 19:16:11 -0400

    adding semi working bar graph

--- pg-prom/server.js
+++ pg-prom/server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 8000;
 
 server.use(express.static('public'));
 server.use('/scripts', express.static(__dirname + '/node_modules/datamaps/dist/'));     // node modules are private
+server.set('views', './views');
+server.set('view engine', 'ejs');
 
 server.use('/all', refugeeRouter.getAllData());
 server.use('/destinations', refugeeRouter.getDestinations());
@@ -17,6 +19,12 @@ server.get('/', (req,res)=>{
 
 server.get('/map', (req, res)=>{
     res.sendFile('map.html', {root:'./public'});
+});
+
+server.get('/bar', function (req,res){
+    res.render('bar', {
+        title: 'Bar Graph'
+    });
 });
 
 server.listen(PORT,()=>{
